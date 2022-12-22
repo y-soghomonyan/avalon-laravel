fbc876be5a53f5884b940525dafffe85778e08fb
yervand.odesk <yervand.odesk@gmail.com>
2022-12-22 18:56:00 +0400

    changes

--- resources/views/user/config/app.blade.php
+++ resources/views/user/config/app.blade.php
@@ -1,23 +1,26 @@
 @include('user.config.header')
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm d-flex flex-column pb-0">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                    Avalon
                 </a>
-             
-                
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="">
-                    @csrf
-                    <input class="btn btn-danger" type="submit" value="loguot">
-                </form>
-                {{ Auth::user()->name }}
+
+                <div class="nav-profile">
+                    <h4>{{ Auth::user()->name }}</h4>
+                    <div class="logout-container">
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="">
+                            @csrf
+                            <input class="btn btn-danger" type="submit" value="loguot">
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="container mt-3">
+                @include('user.menu.menu')
             </div>
         </nav>
 
         <main class="py-4">
-            <div class="container">
-                @include('user.menu.menu')
-            </div>
          <div class="container-fluid">
              <div class="container mt-5">
                  @include('messages')
@@ -30,3 +33,23 @@
         </main>
    
 @include('user.config.footer')
+
+<style>
+    .nav-profile{
+        position: relative;
+    }
+    .logout-container{
+        width: 100%;
+        position: absolute;
+        display: none;
+    }
+    .logout-container.active{
+        display: flex;
+        justify-content: flex-end;
+    }
+</style>
+<script>
+    $('.nav-profile').on('click', function(){
+        $('.logout-container').toggleClass('active');
+    })
+</script>
